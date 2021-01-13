dae3f08d284d8b1d859d28910a74261a5d85d91f
Krista <krista.coding@gmail.com>
2021-01-13 13:15:07 -0500

    update indexdb

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,9 +14,6 @@ const DATA_CACHE_NAME = "data-cache-v1";
 // install
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-    );
-    evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
 
